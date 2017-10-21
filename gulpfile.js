d1220fb994e1be03d6396b9399d8b6b4234c8982
Sina Bahram <sina@sinabahram.com>
2017-10-21 16:58:32 -0400

    added SLOC reporting to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 const gulp = require('gulp');
 const jsValidate = require('gulp-jsvalidate');
+const sloc = require('gulp-sloc');
 
 gulp.task('validate',() =>
     gulp.src(['**/*.js', '!node_modules/**'])
         .pipe(jsValidate())
 );
+
+gulp.task('sloc', function(){
+  gulp.src(['**/*.js', '!node_modules/**'], {nodir: true})
+    .pipe(sloc());
+});
+
+
+
